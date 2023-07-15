fd1b00e9ad55c5a3e9c7c593668be0f3f86fa6c9
Georgi Kostadinov <georgi.kostadinov177@gmail.com>
2023-07-15 18:44:14 +0300

    Add solutions from Reflection and Attributes problems

---
+++ Reflection and Attributes - Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+       
+
+        public string Read(string args)
+        {
+            string[] arguments = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = arguments[0];
+
+            string[] commandArguments = arguments.Skip(1).ToArray();
+
+            Type commandType = Assembly
+                .GetEntryAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == $"{commandName}Command");
+
+            if( commandType == null ) 
+            {
+                throw new InvalidOperationException("Command not found.");
+            }
+
+            ICommand commandInstance = Activator.CreateInstance(commandType) as ICommand;
+            
+            string result = commandInstance.Execute(commandArguments);
+
+            return result.ToString();
+        }
+
+       
+    }
+}
